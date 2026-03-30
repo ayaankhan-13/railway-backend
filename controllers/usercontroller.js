c1eed3bc750701343823560b568de8d01654a581
ayaankhan12-beep <muhammadayaanmuhammadshoaib@gmail.com>
2026-03-31 02:15:39 +0500

    ggg

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -69,7 +69,7 @@ const sendOtp = async (email , Otp) => {
 
 
 const emailOptions = {
-  from: `SignUp message <${process.env.SMTP_PASS}>`,
+  from: `SignUp message <${process.env.SMTP_USER}>`,
   to: email,
   subject: "OTP code for account verification",
   text: `Your OTP is ${Otp}`
